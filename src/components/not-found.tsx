c5a8cf7cbdf360c1c479ff39034fb33054d4216f
Dennis Castillo <denniscastle24@gmail.com>
2025-03-01 19:40:28 -0600

    Config imports from configs

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,12 +1,8 @@
-import TopOptions from "../components/top-options";
 import { Link } from "react-router";
 
 const NotFound = () => {
   return (
     <>
-      <TopOptions>
-        <Link to={"/"}>Home</Link>
-      </TopOptions>
       <div className="h-[90%] flex items-center justify-center py-12">
         <div className="mx-auto w-full max-w-sm space-y-6">
           <h1 className="text-center scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
